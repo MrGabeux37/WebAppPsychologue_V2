87146de1ba6eb96703fe945e60336978e0b61f2f
Gabriel Lacroix <glacroix0@live.ca>
2021-11-01 16:11:58 -0400

    Mods port for app.listen and revert routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+var port = process.env.PORT || 3000;
 var routes = require('./web/base/routes.js');
 
 app.use('/',routes);
@@ -8,4 +8,6 @@ app.use('/',routes);
 //serves static files
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+});
