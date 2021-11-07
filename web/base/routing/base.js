58782255871824762fd8a3208b3fc74c6fd173bc
Gabriel Lacroix <glacroix0@live.ca>
2021-11-07 16:39:50 -0500

    removed mysql and installed mysql2

--- web/base/routing/base.js
+++ web/base/routing/base.js
@@ -1,37 +1,48 @@
 var express = require('express');
-var router = express.Router();
-const crypto = require('crypto');
+var Router = express.Router();
+const Crypto = require('crypto');
+const Sequelize =require('sequelize');
+const Op = Sequelize.Op;
+const Client = require('../models/client.js');
+const Psychologue = require('../models/psychologue.js');
+const PlageHoraire = require('../models/plagehoraire.js');
+const RendezVous = require('../models/rendezvous.js');
 
 //hash password method
 const getHashedPassword = (password) => {
-  const sha256 = crypto.createHash('sha256');
+  const sha256 = Crypto.createHash('sha256');
   const hash = sha256.update(password).digest('base64');
   return hash;
 }
 
 //testing Route
-router.get('/test', function(req, res){
+Router.get('/test', function(req, res){
   res.render('../public/views/psychologue/reservations', {layout: 'psychologue'})
 })
 //first route
-router.get('/', function(req, res){
+Router.get('/', function(req, res){
   res.redirect('/login');
 })
 
+//Post login
+Router.post('/', function(req, res){
+
+})
+
 //login page
-router.get('/login', function(req, res){
+Router.get('/login', function(req, res){
   res.render('../public/views/main/login', {layout:'main'})
 });
 
 //register page
-router.get('/register', function(req, res){
+Router.get('/register', function(req, res){
   res.render('../public/views/main/register', {layout: 'main'})
 })
 
 //basic POST route
-router.post('/', function(req, res){
+Router.post('/', function(req, res){
   res.send('POST default route');
 });
 
-//export this router to use in index.js
-module.exports = router;
+//export this Router to use in index.js
+module.exports = Router;
