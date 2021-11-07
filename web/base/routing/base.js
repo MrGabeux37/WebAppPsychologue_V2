e4cf6b42d2fdebd4903d35540091a42e363b9567
Gabriel Lacroix <glacroix0@live.ca>
2021-11-07 17:36:53 -0500

    added account recognition in POST /login route
    Displays a message recognizing the type of account or if the account is not found.

--- web/base/routing/base.js
+++ web/base/routing/base.js
@@ -16,31 +16,69 @@ const getHashedPassword = (password) => {
 }
 
 //testing Route
-Router.get('/test', function(req, res){
+Router.get('/test',async function(req, res){
   res.render('../public/views/psychologue/reservations', {layout: 'psychologue'})
 })
 //first route
-Router.get('/', function(req, res){
+Router.get('/',async function(req, res){
   res.redirect('/login');
 })
 
-//Post login
-Router.post('/', function(req, res){
-
-})
-
 //login page
-Router.get('/login', function(req, res){
+Router.get('/login',async function(req, res){
   res.render('../public/views/main/login', {layout:'main'})
 });
 
+//Post login
+Router.post('/login', async function(req, res){
+  console.log(req.body.inputCourriel);
+  var scope="";
+  var inputCourriel = req.body.inputCourriel;
+  var inputPassword = req.body.inputPassword;
+
+  //cherche un utilisateur dans la table client avec le courriel entré
+  var user=await Client.findOne({
+    where:{courriel: inputCourriel}
+  });
+
+  //verifie s'il la requete précédente donne un résultat null
+  if(!user){
+    //cherche un utilisateur dans la table psychologue avec le courriel entré
+    user=await Psychologue.findOne({
+      where:{courriel:inputCourriel}
+    })
+    //verifie s'il la requete précédente donne un résultat null
+    if(!user){
+      res.render('../public/views/main/login', {
+        layout:'main',
+        message: 'Compte introuvable',
+        messageClass: 'alert-danger'
+      });
+    }
+    else{
+      res.render('../public/views/main/login', {
+        layout:'main',
+        message: 'Compte psychologue',
+        messageClass: 'alert-success'
+      });
+    }
+  }
+  else{
+    res.render('../public/views/main/login', {
+      layout:'main',
+      message: 'Compte client',
+      messageClass: 'alert-success'
+    });
+  }
+})
+
 //register page
-Router.get('/register', function(req, res){
+Router.get('/register',async function(req, res){
   res.render('../public/views/main/register', {layout: 'main'})
 })
 
 //basic POST route
-Router.post('/', function(req, res){
+Router.post('/',async function(req, res){
   res.send('POST default route');
 });
 
