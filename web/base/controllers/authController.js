c55a34c71d450cf2285011c27166ee45ebfb245e
Gabriel Lacroix <glacroix0@live.ca>
2021-11-11 18:04:11 -0500

    Separated login and register handlers in authController
    reduced the amount of code in base routes

---
+++ web/base/controllers/authController.js
@@ -0,0 +1,158 @@
+const Client = require('../models/client.js');
+const Psychologue = require('../models/psychologue.js');
+const jwt = require('jsonwebtoken');
+const maxAge = 3 * 24 * 60 * 60;
+
+const createToken = (id) => {
+  return jwt.sign({ id }, 'Le Prince des Petits', {
+    expiresIn: maxAge
+  });
+}
+
+module.exports.register_post = async (req, res) => {
+  try{
+    //initialisation des variables
+    var payload = req.body;
+    var parent1, parent2, enfant;
+    var password_parent1=null;
+    var password_parent2=null;
+    var password_enfant=null;
+    var courriel_enfant=null;
+    //encryption du mot de passe
+    var password = getHashedPassword(payload.mot_de_passe[1]);
+    if(payload.courriel_utilise1)password_parent1=password;
+    if(payload.courriel_utilise2)password_parent2=password;
+    if(payload.courriel_utilise3)password_enfant=password;
+    if(payload.courrielcheckenfant!=true){
+      courriel_enfant=payload.courriel_enfant;
+      password_enfant=password;
+    }
+    //creation de l'objet enfant
+    enfant = await Client.build({
+      nom:payload.nom_enfant,
+      prenom:payload.prenom_enfant,
+      date_de_naissance:payload.date_de_naissance_enfant,
+      sexe:payload.sexe_enfant,
+      courriel:courriel_enfant,
+      num_telephone:payload.num_telephone_parent1,
+      permission:false,
+      mot_de_passe:password_enfant,
+      id_parent1:null,
+      id_parent2:null
+    });
+    enfant.save();
+
+    //creation de l'objet parent1
+    parent1 = await Client.build({
+      nom:payload.nom_parent1,
+      prenom:payload.prenom_parent1,
+      date_de_naissance:payload.date_de_naissance_parent1,
+      sexe:payload.sexe_parent1,
+      courriel:payload.courriel_parent1,
+      num_telephone:payload.num_telephone_parent1,
+      permission:false,
+      mot_de_passe:password_parent1
+    })
+    //sauvegarde dans la bd et ajout de id_parent1 à l'enfant
+    parent1.save().then(function(id1){
+      enfant.update({
+        id_parent1:id1.id_client
+      });
+    });
+
+    if(!payload.famillecheck){
+      //creation de l'objet parent2
+      parent2 = await Client.build({
+        nom:payload.nom_parent2,
+        prenom:payload.prenom_parent2,
+        date_de_naissance:payload.date_de_naissance_parent2,
+        sexe:payload.sexe_parent2,
+        courriel:payload.courriel_parent2,
+        num_telephone:payload.num_telephone_parent2,
+        permission:false,
+        mot_de_passe:password_parent2
+      });
+      //sauvegarde dans la bd et ajout de id_parent2 à l'enfant
+      parent2.save().then(function(id2){
+        enfant.update({
+          id_parent2:id2.id_client
+        });
+      });
+    }
+    res.redirect('/');
+  }catch (err){
+    //check to create a error handler function for different errors
+    //const errors = handleErrors(err);
+    console.log(err);
+  }
+};
+
+module.exports.login_post = async (req, res) => {
+  try{
+    var inputCourriel = req.body.inputCourriel;
+    var inputPassword = getHashedPassword(req.body.inputPassword);
+    console.log(inputPassword)
+
+    //cherche un utilisateur dans la table client avec le courriel entré
+    var user=await Client.findOne({
+      where:{courriel: inputCourriel}
+    });
+
+    //verifie s'il la requete précédente donne un résultat null
+    if(!user){
+      //cherche un utilisateur dans la table psychologue avec le courriel entré
+      user=await Psychologue.findOne({
+        where:{courriel:inputCourriel}
+      })
+      //verifie s'il la requete précédente donne un résultat null
+      if(!user){
+        res.render('../public/views/main/login', {
+          layout:'main',
+          message: 'Compte introuvable',
+          messageClass: 'alert-danger'
+        });
+      }
+      //si compte psy est vrai
+      else{
+        //si mot de passe est correct
+        if(inputPassword==user.mot_de_passe){
+          res.cookie('userID',user.id_psychologue);
+          res.cookie('scope','psychologue');
+          res.redirect('/profil_psychologue');
+
+        }
+        //si mot de passe est pas bon pour psy
+        else{
+          res.render('../public/views/main/login', {
+            layout:'main',
+            message: 'Mauvais mot de passe',
+            messageClass: 'alert-danger'
+          });
+        }
+      }
+    }
+    //si compte client est vrai
+    else{
+      //si mot de passe est correct pour client
+      if(inputPassword==user.mot_de_passe){
+        var scope = (user.permission) ? 'clientOui' : 'clientNon';
+        res.cookie('userID',user.id_client);
+        res.cookie('scope',scope);
+        //change to redirect
+        res.redirect('/profil_client');
+      }
+      //mot de passe pas bon
+      else{
+        res.render('../public/views/main/login', {
+          layout:'main',
+          message: 'Mauvais mot de passe',
+          messageClass: 'alert-danger'
+        });
+      }
+    }
+  }catch(err){
+    //check to create a error handler function for different errors
+    //const errors = handleErrors(err);
+    console.log(err);
+  }
+}
--- web/base/routing/base.js
+++ web/base/routing/base.js
@@ -7,6 +7,7 @@ const Client = require('../models/client.js');
 const Psychologue = require('../models/psychologue.js');
 const PlageHoraire = require('../models/plagehoraire.js');
 const RendezVous = require('../models/rendezvous.js');
+const authController = require('../controllers/authController.js');
 
 //hash password method
 const getHashedPassword = (password) => {
@@ -40,69 +41,7 @@ Router.get('/login',async function(req, res){
 });
 
 //Post login
-Router.post('/login', async function(req, res){
-  var inputCourriel = req.body.inputCourriel;
-  var inputPassword = getHashedPassword(req.body.inputPassword);
-  console.log(inputPassword)
-
-  //cherche un utilisateur dans la table client avec le courriel entré
-  var user=await Client.findOne({
-    where:{courriel: inputCourriel}
-  });
-
-  //verifie s'il la requete précédente donne un résultat null
-  if(!user){
-    //cherche un utilisateur dans la table psychologue avec le courriel entré
-    user=await Psychologue.findOne({
-      where:{courriel:inputCourriel}
-    })
-    //verifie s'il la requete précédente donne un résultat null
-    if(!user){
-      res.render('../public/views/main/login', {
-        layout:'main',
-        message: 'Compte introuvable',
-        messageClass: 'alert-danger'
-      });
-    }
-    //si compte psy est vrai
-    else{
-      //si mot de passe est correct
-      if(inputPassword==user.mot_de_passe){
-        res.cookie('userID',user.id_psychologue);
-        res.cookie('scope','psychologue');
-        res.redirect('/profil_psychologue');
-
-      }
-      //si mot de passe est pas bon pour psy
-      else{
-        res.render('../public/views/main/login', {
-          layout:'main',
-          message: 'Mauvais mot de passe',
-          messageClass: 'alert-danger'
-        });
-      }
-    }
-  }
-  //si compte client est vrai
-  else{
-    //si mot de passe est correct pour client
-    if(inputPassword==user.mot_de_passe){
-      var scope = (user.permission) ? 'clientOui' : 'clientNon';
-      res.cookie('userID',user.id_client);
-      res.cookie('scope',scope);
-      //change to redirect
-      res.redirect('/profil_client');
-    }
-    //mot de passe pas bon
-    else{
-      res.render('../public/views/main/login', {
-        layout:'main',
-        message: 'Mauvais mot de passe',
-        messageClass: 'alert-danger'
-      });
-    }
-  }
-});
+Router.post('/login', authController.login_post);
 
 //register page
 Router.get('/register',async function(req, res){
@@ -113,77 +52,7 @@ Router.get('/register',async function(req, res){
 });
 
 //POST register
-Router.post('/register',async function(req, res){
-  //initialisation des variables
-  var payload = req.body;
-  var parent1, parent2, enfant;
-  var password_parent1=null;
-  var password_parent2=null;
-  var password_enfant=null;
-  var courriel_enfant=null;
-  //encryption du mot de passe
-  var password = getHashedPassword(payload.mot_de_passe[1]);
-  if(payload.courriel_utilise1)password_parent1=password;
-  if(payload.courriel_utilise2)password_parent2=password;
-  if(payload.courriel_utilise3)password_enfant=password;
-  if(payload.courrielcheckenfant!=true){
-    courriel_enfant=payload.courriel_enfant;
-    password_enfant=password;
-  }
-  //creation de l'objet enfant
-  enfant = await Client.build({
-    nom:payload.nom_enfant,
-    prenom:payload.prenom_enfant,
-    date_de_naissance:payload.date_de_naissance_enfant,
-    sexe:payload.sexe_enfant,
-    courriel:courriel_enfant,
-    num_telephone:payload.num_telephone_parent1,
-    permission:false,
-    mot_de_passe:password_enfant,
-    id_parent1:null,
-    id_parent2:null
-  });
-  enfant.save();
-
-  //creation de l'objet parent1
-  parent1 = await Client.build({
-    nom:payload.nom_parent1,
-    prenom:payload.prenom_parent1,
-    date_de_naissance:payload.date_de_naissance_parent1,
-    sexe:payload.sexe_parent1,
-    courriel:payload.courriel_parent1,
-    num_telephone:payload.num_telephone_parent1,
-    permission:false,
-    mot_de_passe:password_parent1
-  })
-  //sauvegarde dans la bd et ajout de id_parent1 à l'enfant
-  parent1.save().then(function(id1){
-    enfant.update({
-      id_parent1:id1.id_client
-    });
-  });
-
-  if(!payload.famillecheck){
-    //creation de l'objet parent2
-    parent2 = await Client.build({
-      nom:payload.nom_parent2,
-      prenom:payload.prenom_parent2,
-      date_de_naissance:payload.date_de_naissance_parent2,
-      sexe:payload.sexe_parent2,
-      courriel:payload.courriel_parent2,
-      num_telephone:payload.num_telephone_parent2,
-      permission:false,
-      mot_de_passe:password_parent2
-    });
-    //sauvegarde dans la bd et ajout de id_parent2 à l'enfant
-    parent2.save().then(function(id2){
-      enfant.update({
-        id_parent2:id2.id_client
-      });
-    });
-  }
-  res.redirect('/');
-});
+Router.post('/register',authController.register_post);
 
 //route de deconnection
 Router.get('/logout', async function(req, res){
