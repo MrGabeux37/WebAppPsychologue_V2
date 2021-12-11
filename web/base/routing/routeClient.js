af7aa4dc7465de6267731d7a902ed4567485ee91
Gabriel Lacroix <glacroix0@live.ca>
2021-12-10 19:57:28 -0500

    optimized client profile route
    removed duplicate codes
    Added password change button to redirect to password change page

--- web/base/routing/routeClient.js
+++ web/base/routing/routeClient.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const Client = require('../models/client.js');
+const Contact = require('../models/contact.js');
+const Psychologue = require('../models/psychologue.js');
 const jwt = require('jsonwebtoken');
 const authController = require('../controllers/authController.js');
 
@@ -8,10 +11,53 @@ router.get('/profil_client',async function(req, res){
   var decoded = authController.decodeCookie(req.cookies.jwt);
   console.log(decoded);
   if(decoded.scope=='clientOui'){
-    res.render('../public/views/client/profil', {layout:'clientOui'});
+
+    //get contact
+    var contact = await Contact.findOne({
+      where:{ID_Contact: decoded.id}
+    });
+
+    //get client of Contact
+    var client = await Client.findOne({
+      where:{id_client : contact.Client}
+    });
+
+    res.render('../public/views/client/profil',{
+      layout:'clientOui',
+      nomContact: contact.Nom,
+      prenomContact: contact.Prenom,
+      courrielContact: contact.Email,
+      numTelContact: contact.Num_Tel,
+      courriel_extra1: contact.Email_Extra1,
+      courriel_extra2: contact.Email_Extra2,
+      courriel_extra3: contact.Email_Extra3,
+      enfantNom: client.nom,
+      enfantPrenom: client.prenom
+    });
   }
   else if(decoded.scope=='clientNon'){
-    res.render('../public/views/client/profil', {layout:'clientNon'});
+    //get contact
+    var contact = await Contact.findOne({
+      where:{ID_Contact: decoded.id}
+    });
+
+    //get client of Contact
+    var client = await Client.findOne({
+      where:{id_client : contact.Client}
+    });
+
+    res.render('../public/views/client/profil', {
+      layout:'clientNon',
+      nomContact: contact.Nom,
+      prenomContact: contact.Prenom,
+      courrielContact: contact.Email,
+      numTelContact: contact.Num_Tel,
+      courriel_extra1: contact.Email_Extra1,
+      courriel_extra2: contact.Email_Extra2,
+      courriel_extra3: contact.Email_Extra3,
+      enfantNom: client.nom,
+      enfantPrenom: client.prenom
+    });
   }
   else{
     res.redirect('/errorAccess');
