dc1dbf616673589fd1faaa587195018c6f97cc40
Gabriel Lacroix <glacroix0@live.ca>
2022-02-08 18:03:29 -0500

    Clients initial page added
    generating the list of clients
    search engine implemented for clients display

--- web/base/routing/psychologue/routePsycho.js
+++ web/base/routing/psychologue/routePsycho.js
@@ -3,8 +3,13 @@ var router = express.Router();
 const Client = require('../../models/client.js');
 const Contact = require('../../models/contact.js');
 const Psychologue = require('../../models/psychologue.js');
+const RendezVous = require('../../models/rendezvous.js');
+const PlageHoraire = require('../../models/plagehoraire.js');
+const Sequelize =require('sequelize');
+const Op = Sequelize.Op;
 const jwt = require('jsonwebtoken');
 const authController = require('../../controllers/authController.js');
+const url = require('url');
 
 //get profil du psychologue
 router.get('/profil_psychologue',async function(req, res){
@@ -63,7 +68,6 @@ router.get('/profil_psychologue/update', async function(req,res){
 //update info du psychologue dans la db
 router.post('/profil_psychologue/update', async function(req,res){
   var decoded = authController.decodeCookie(req.cookies.jwt);
-  console.log(decoded);
   if(decoded.scope=='psychologue'){
     //get payload
     var payload = req.body;
@@ -86,11 +90,24 @@ router.post('/profil_psychologue/update', async function(req,res){
 
 router.get('/clients', async function(req,res){
   var decoded = authController.decodeCookie(req.cookies.jwt);
-  console.log(decoded);
   if(decoded.scope=='psychologue'){
+    //get url query
+    const query = url.parse(req.url, true).query;
+    console.log(query);
 
+    //get client with or without query info
+    var clients = await Client.findAll({
+      where:{
+        nom:{[Op.substring]:(query.nom_client==undefined ? '' : query.nom_client)},
+        prenom:{[Op.substring]:(query.prenom_client==undefined ? '' : query.prenom_client)}
+      }
+    });
+    console.log(clients);
     res.render('../public/views/psychologue/clients_recherche', {
-      layout:'psychologue'
+      layout:'psychologue',
+      nom : query.nom_client,
+      prenom: query.prenom_client,
+      resultats:clients
     });
   }
   else{
