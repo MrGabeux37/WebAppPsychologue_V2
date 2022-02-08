a424b47b1397d076bdc850d8a75f266438bc82d5
Gabriel Lacroix <glacroix0@live.ca>
2022-02-08 16:59:41 -0500

    changed for the correct page
    chose the wrong html on the earlier commit

--- web/base/routing/psychologue/routePsycho.js
+++ web/base/routing/psychologue/routePsycho.js
@@ -89,7 +89,7 @@ router.get('/clients', async function(req,res){
   console.log(decoded);
   if(decoded.scope=='psychologue'){
 
-    res.render('../public/views/psychologue/clients_recherche', {
+    res.render('../public/views/psychologue/clients', {
       layout:'psychologue'
     });
   }
