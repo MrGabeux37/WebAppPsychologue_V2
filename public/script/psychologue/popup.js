d7730f1a7c2ef7b710109839411b296027585079
Gabriel Lacroix <glacroix0@live.ca>
2022-03-06 15:55:42 -0500

    Set up the modal for the new reservation
    added conditions to generate the form
    Next step will be to provide the information with the calendar route

--- public/script/psychologue/popup.js
+++ public/script/psychologue/popup.js
@@ -1,6 +1,1 @@
-var myModal = document.getElementById('exampleModal')
-var myInput = document.getElementById('myInput')
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
